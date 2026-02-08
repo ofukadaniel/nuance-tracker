d7eb83b7726f6b586fca0b052fb038a5732ee2e9
ofukadaniel <ofukadaniel@gmail.com>
2026-02-07 20:55:26 -0800

    Add files via upload

---
+++ sw.js.js
@@ -0,0 +1,18 @@
+const CACHE = "nuance-cache-v1";
+const ASSETS = [
+  "/",
+  "/index.html",
+  "/manifest.webmanifest",
+  "/icon-192.png",
+  "/icon-512.png"
+];
+
+self.addEventListener("install", (e) => {
+  e.waitUntil(caches.open(CACHE).then((c) => c.addAll(ASSETS)));
+});
+
+self.addEventListener("fetch", (e) => {
+  e.respondWith(
+    caches.match(e.request).then((res) => res || fetch(e.request))
+  );
+});
